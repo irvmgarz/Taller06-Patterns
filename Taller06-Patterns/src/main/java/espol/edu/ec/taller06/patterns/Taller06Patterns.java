c07cf1efb3f83852cd352ecb87582e98ff32385c
Christopher Acosta <chriacos@espol.edu.ec>
2022-12-11 22:52:21 -0500

    projecto java vacío

---
+++ Taller06-Patterns/src/main/java/espol/edu/ec/taller06/patterns/Taller06Patterns.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package espol.edu.ec.taller06.patterns;
+
+/**
+ *
+ * @author chris
+ */
+public class Taller06Patterns {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
